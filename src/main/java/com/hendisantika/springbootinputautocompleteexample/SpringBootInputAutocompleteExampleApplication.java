59816806e33451c52e8f7021be12f71a7436167e
Hendi Santika <hendisantika@gmail.com>
2018-08-25 06:31:24 +0700

    #1 - first commit

---
+++ src/main/java/com/hendisantika/springbootinputautocompleteexample/SpringBootInputAutocompleteExampleApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springbootinputautocompleteexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInputAutocompleteExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootInputAutocompleteExampleApplication.class, args);
+    }
+}
---
+++ src/test/java/com/hendisantika/springbootinputautocompleteexample/SpringBootInputAutocompleteExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.springbootinputautocompleteexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootInputAutocompleteExampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
